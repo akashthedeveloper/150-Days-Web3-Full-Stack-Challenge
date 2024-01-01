c432a5c6f41b16d9f7ce4476c7a9fdf370a8ff3f
Akash Jha <49565671+akashthedeveloper@users.noreply.github.com>
2024-01-01 11:16:34 +0530

    canvas

---
+++ Web Dev/Canvas/script.js
@@ -0,0 +1,29 @@
+// 1) For Line
+
+var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
+ctx.moveTo(0, 0);
+ctx.lineTo(200, 100);
+ctx.stroke();
+
+// 2) for circle
+
+var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
+ctx.beginPath();
+ctx.arc(95, 50, 40, 0, 2 * Math.PI);
+ctx.stroke();
+
+// 3) Draw linear gradient
+
+var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
+
+// Create gradient
+var grd = ctx.createRadialGradient(75, 50, 5, 90, 60, 100);
+grd.addColorStop(0, "red");
+grd.addColorStop(1, "white");
+
+// Fill with gradient
+ctx.fillStyle = grd;
+ctx.fillRect(10, 10, 150, 80);
